5ae69db4bf4e8c648a1896f290bcb748e75e2ae8
Ayeps <ayeps2000@gmail.com>
2016-06-03 09:57:12 +0000

    testing templates

--- app.js
+++ app.js
@@ -65,6 +65,65 @@ controller.on('facebook_option', function (bot, message) {
         }
     })
 });
+controller.hears(['show'], function (bot, message) {
+    bot.reply(message, 'Hello');
+    bot.reply(message, 'Hi, my name is Pepper and I am your Black Jack Dealer.Would you like to play a round?!');
+    bot.reply(message, {
+        attachment: {
+            type: "template",
+            payload: {
+                template_type: "generic",
+                elements: [
+                    {
+                        title: "Classic White T-Shirt",
+                        image_url: "http://petersapparel.parseapp.com/img/item100-thumb.png",
+                        subtitle: "Soft white cotton t-shirt is back in style",
+                        buttons: [
+                            {
+                                type: "web_url",
+                                url: "https://petersapparel.parseapp.com/view_item?item_id=100",
+                                title: "View Item"
+                            },
+                            {
+                                type: "web_url",
+                                url: "https://petersapparel.parseapp.com/buy_item?item_id=100",
+                                title: "Buy Item"
+                            },
+                            {
+                                type: "postback",
+                                title: "Bookmark Item",
+                                payload: "USER_DEFINED_PAYLOAD_FOR_ITEM100"
+                            }
+                        ]
+                    },
+                    {
+                        title: "Classic Grey T-Shirt",
+                        image_url: "http://petersapparel.parseapp.com/img/item101-thumb.png",
+                        subtitle: "Soft gray cotton t-shirt is back in style",
+                        buttons: [
+                            {
+                                type: "web_url",
+                                url: "https://petersapparel.parseapp.com/view_item?item_id=101",
+                                title: "View Item"
+                            },
+                            {
+                                type: "web_url",
+                                url: "https://petersapparel.parseapp.com/buy_item?item_id=101",
+                                title: "Buy Item"
+                            },
+                            {
+                                type: "postback",
+                                title: "Bookmark Item",
+                                payload: "USER_DEFINED_PAYLOAD_FOR_ITEM101"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    });
+})
+
 
 controller.hears(['hello', 'hi'], 'message_received', function (bot, message) {
     bot.reply(message, 'Hello');
