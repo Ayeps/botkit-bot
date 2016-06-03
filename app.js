40e4ae27dc1c993c17f2fa7c95110e7dd1ec2d3d
Ayeps <ayeps2000@gmail.com>
2016-06-03 08:20:14 +0000

    removing convo

--- app.js
+++ app.js
@@ -67,16 +67,16 @@ controller.on('facebook_option', function (bot, message) {
 });
 
 
-controller.hears(['cookies'], 'message_received', function (bot, message) {
-
-    bot.startConversation(message, function (err, convo) {
-        convo.say('Did someone say cookies!?!!');
-        convo.ask('What is your favorite type of cookie?', function (response, convo) {
-            convo.say('Golly, I love ' + response.text + ' too!!!');
-            convo.next();
-        });
-    });
-});
+//controller.hears(['cookies'], 'message_received', function (bot, message) {
+//
+//    bot.startConversation(message, function (err, convo) {
+//        convo.say('Did someone say cookies!?!!');
+//        convo.ask('What is your favorite type of cookie?', function (response, convo) {
+//            convo.say('Golly, I love ' + response.text + ' too!!!');
+//            convo.next();
+//        });
+//    });
+//});
 
 
 controller.on('facebook_postback', function (bot, message) {
